45dfbe5246d99d620c96d8966c208888d2f25e4b
Manuel Kuhlmann <manuel@mkuhlmann.org>
2019-06-30 07:22:04 -0400

    Trying to fix blockquote but frontent config is fucked up in neos

--- DistributionPackages/mkuhlmann.ImpfDich/Resources/Public/Backend/CkEditorStyling.js
+++ DistributionPackages/mkuhlmann.ImpfDich/Resources/Public/Backend/CkEditorStyling.js
@@ -1,32 +1,63 @@
-(function() {
-	
+/*(function() {
 
-	window['@Neos:HostPluginAPI']['@manifest']('main', {}, function(globalRegistry) {
+	window['@Neos:HostPluginAPI']['@manifest']('mkuhlmann.ImpfDich:CkBlockquote', {}, function(globalRegistry) {
 
+		window.__neosVendor = window["@Neos:HostPluginAPI"]["@vendor"]();
+		window.__neosRegistry = globalRegistry;
+		
+		
 		var reactUiComponents = window['@Neos:HostPluginAPI']['@NeosProjectPackages']().ReactUiComponents;
 
 
-		var ckEditorRegistry = globalRegistry.get('ckEditor');
+		var ckEditorRegistry = globalRegistry.get('ckEditor5');
 		var richtextToolbar = ckEditorRegistry.get('richtextToolbar');
 
-		richtextToolbar.set('mkuhlmann.ImpfDich:CkBlockquoteButton', {
-			formattingRule: 'mkuhlmann.ImpfDich:CkBlockquote',
+		richtextToolbar.set('mkuhlmann.ImpfDich:CkBlockquote', {
+			commandName: 'blockquoteCommand',
+			isActive: __neosVendor.plow.$get('blockquoteCommand'),
+			isVisible: __neosVendor.plow.$get(['formatting', 'mkuhlmann.ImpfDich:CkBlockquote']),
+			
 			component: reactUiComponents.IconButton,
-			callbackPropName: 'onClick',
-
+			
 			icon: 'quote-right',
-			hoverStyle: 'brand'
+			hoverStyle: 'brand',
+			tooltip: 'Blockquote'
+		});
+		
+		class BlockquoteCommand extends __neosVendor.CkEdito5.Command {
+			
+		}
+		
+		class BlockquotePlugin extends __neosVendor.CkEditor5.Plugin {
+			static get pluginName() {
+				return 'mkuhlmann.ImpfDich:CkBlockquote';
+			}
+			
+			init() {
+				this.editor.model.schema.register('blockquote', {
+					isObject: true,
+				});
+				
+				this.editor.conversion.elementFromPoint({
+					model: 'blockquote',
+					view: {
+						name: 'blockquote'
+					}
+				});
+				
+				this.editor.commands.add('blockquoteCommand', new 
+			}
+		}
+		
+		ckEditorRegistry.get('config').set('mkuhlmann.ImpfDich:CkBlockquote', (ckEditorConfiguration, options) => {
+			__neosVendor.plow.$add('plugins', BlockquotePlugin, ckEditorConfiguration);
+			return ckEditorConfiguration;			
 		});
 
-		var formattingRules = ckEditorRegistry.get('formattingRules');
-
-		formattingRules.set('mkuhlmann.ImpfDich:CkBlockquote', {
-			 command: 'blockquote',
-			 config: formattingRules.config.add('Blockquote')
-		});
+		
 	});
 
-	console.log('yo');
 
 	
 })();
+*/
